5cd769471c11daa93baf7d3ea0732bc518f272dc
fcbsousa20 <fcbsousa20@gmail.com>
2025-03-07 23:48:27 -0300

    salvar

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -19,8 +19,8 @@ resource "aws_iam_policy" "codepipeline_s3_policy" {
           "s3:ListBucket"
         ],
         Resource = [
-          "arn:aws:s3:::bucket-devsecops-camila",
-          "arn:aws:s3:::bucket-devsecops-camila/*"
+          "arn:aws:s3:::sousabuct1999",
+          "arn:aws:s3:::sousabuct1999/*"
         ]
       }
     ]
